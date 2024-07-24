c7f92c24dd007fea885c7fe3dcf3f66fd7372e7c
MateoMartindev <emateomartinb@gmail.com>
2024-07-23 20:09:06 -0500

    Margenes y widths de main

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,41 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			colors: {
-				'text-1': '#24223e'
-			},
+	  extend: {
+		colors: {
+		  'text-1': '#24223e',
 		},
+		typography: (theme) => ({
+		  DEFAULT: {
+			css: {
+			  h1: {
+				fontSize: 'initial',
+				fontWeight: 'initial',
+			  },
+			  h2: {
+				fontSize: 'initial',
+				fontWeight: 'initial',
+			  },
+			  h3: {
+				fontSize: 'initial',
+				fontWeight: 'initial',
+			  },
+			  h4: {
+				fontSize: 'initial',
+				fontWeight: 'initial',
+			  },
+			  h5: {
+				fontSize: 'initial',
+				fontWeight: 'initial',
+			  },
+			  h6: {
+				fontSize: 'initial',
+				fontWeight: 'initial',
+			  },
+			},
+		  },
+		}),
+	  },
 	},
-	plugins: [],
-}
+	plugins: [require('@tailwindcss/typography')],
+  }
